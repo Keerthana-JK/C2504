ca9309b68ba7a673368f40572f40272c51ed6c29
Keerthana <keerthanajayalalk@gmail.com>
2024-08-02 16:15:46 +0530

    P1
    Print given series using
    Q1. for loop,
    Q2. while loop and
    Q.3 do-while loop

---
+++ 02-08-24/P1/Q1/Program.cs
@@ -0,0 +1,49 @@
+﻿//P1.Q1
+//Print series 10 12 14 16 18... using For Loop
+
+using System;
+namespace Q1
+{
+    internal class Program
+    {
+        static void PrintSeries10_12_14(int numOfTerms)
+        {
+            int term = 10;
+            for (int I = 1; I <= numOfTerms; I++)
+            {
+                Console.Write($"{term} ");
+                term = term + 2;
+            }
+        }
+        // input=5, output=10 12 14 16 18
+        // input=8, output=10 12 14 16 18 20 22 24
+        // input=3, output=10 12 14
+        static void TestPrintSeries10_12_14()
+        {
+            Console.Write("Enter number of terms:");
+            int numOfTerms = int.Parse(Console.ReadLine());
+            Console.Write("Series: ");
+            PrintSeries10_12_14(numOfTerms);
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args) //user: p
+        {
+            Console.WriteLine("--------------TestPrintSeries10_12_14...--------------");
+            TestPrintSeries10_12_14();
+            Console.WriteLine("--------------End TestPrintSeries10_12_14...--------------");
+            Console.WriteLine("Press any key to contine...");
+            Console.ReadKey();
+        }
+        //static void Main(string[] args)
+        //{   
+        //    //continue
+        //    for (int i = 1; i <= 10; i++)
+        //    {
+        //        if (i % 2 != 0)
+        //            continue;
+        //        Console.WriteLine(i);
+        //    }
+        //}
+    }
+}
