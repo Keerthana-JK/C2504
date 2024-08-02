ceca4cd0f40d01695df6705aa63786eaed72ce65
Keerthana <keerthanajayalalk@gmail.com>
2024-08-02 18:00:21 +0530

    P5

---
+++ 02-08-24/P5/Q1/Program.cs
@@ -0,0 +1,43 @@
+﻿//P5.Q1
+//print the N th term in the series 0, 1, 1, 2, 3, 5, 8, 13, ... N terms (using for loop)
+
+using System;
+
+namespace Q1
+{
+    internal class Program
+    {
+        static int Fibonacci(int n)
+        {
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+
+            int fib1 = 0;
+            int fib2 = 1;
+            int fib=0;
+            int i = 0;
+            for (i = 2; i <= n; i++)
+            {
+                fib = fib1 + fib2;
+                fib1 = fib2;
+                fib2 = fib;
+
+            }
+            Console.Write($" {fib}");
+            return fib;
+        }
+
+        static void TestFibonacci()
+        {
+            Console.Write("Enter number of terms required:");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write($"{n}th term :");
+            Fibonacci(n);
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            TestFibonacci();
+        }
+    }
+}
---
+++ 02-08-24/P5/Q2/Program.cs
@@ -0,0 +1,43 @@
+﻿//P5.Q2
+//print the N th term in the series 0, 1, 1, 2, 3, 5, 8, 13, ... N terms (using while loop)
+
+using System;
+
+namespace Q2
+{
+    internal class Program
+    {
+        static int Fibonacci(int n)
+        {
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+
+            int fib1 = 0;
+            int fib2 = 1;
+            int fib = 0;
+            int i = 2;
+            while(i <= n)
+            {
+                fib = fib1 + fib2;
+                fib1 = fib2;
+                fib2 = fib;
+                i++;
+            }
+            Console.Write($" {fib}");
+            return fib;
+        }
+
+        static void TestFibonacci()
+        {
+            Console.Write("Enter number of terms required:");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write($"{n}th term :");
+            Fibonacci(n);
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            TestFibonacci();
+        }
+    }
+}
