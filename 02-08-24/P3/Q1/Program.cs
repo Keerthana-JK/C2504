ddca2eca292cd4678c458549af97354ebbb93688
Keerthana <keerthanajayalalk@gmail.com>
2024-08-02 16:38:01 +0530

    P1

---
+++ 02-08-24/P3/Q1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace Q1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //continue
+            for (int i = 1; i <= 10; i++)
+            {
+            if (i % 2 != 0)
+                continue;
+            Console.WriteLine(i);
+
+            }
+        }
+    }
+}
