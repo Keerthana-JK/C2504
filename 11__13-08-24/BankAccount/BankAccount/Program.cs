916431d744e9789f5a67b6c98c3770a0b6c1d722
Keerthana <keerthanajayalalk@gmail.com>
2024-08-13 15:53:52 +0530

    OOPS - Bank Account

--- 11__13-08-24/BankAccount/BankAccount/Program.cs
+++ 11__13-08-24/BankAccount/BankAccount/Program.cs
@@ -4,8 +4,24 @@ namespace BankAccount
 {
     class Account 
     {
-        string number;
-        float balance;
+        public string number;
+        private float balance;
+
+        public float Balance
+        {
+            private set {
+                if (value < 0 )
+                {
+                    Console.WriteLine("You cannot deposit a negative amount");
+                }
+                if (value > 5000000)
+                {
+                    Console.WriteLine("You cannot deposit amount more than 5000000");
+                }
+                balance = value; 
+            }
+            get { return balance; }
+        }
         public Account(string _number)
         {
             number = _number;
@@ -13,11 +29,11 @@ namespace BankAccount
         }
         public void Deposit(float amount)
         {
-            if(amount>1000000)
-            {
-                Console.WriteLine("You cannot deposit more than 10 Lakhs");
-                return;
-            }
+            //if(amount>1000000)
+            //{
+            //    Console.WriteLine("You cannot deposit more than 10 Lakhs");
+            //    return;
+            //}
             balance += amount;
         }
         public void Withdraw(float amount)
@@ -40,10 +56,12 @@ namespace BankAccount
         static void Main(string[] args)
         {
             Account keerthanaAcc = new Account("1001200230034");
-            keerthanaAcc.Deposit(10000000);
+            keerthanaAcc.Deposit(7000000);
             keerthanaAcc.Deposit(25000);
             keerthanaAcc.Withdraw(15000);
-            Console.WriteLine($"Keerthana Acc balance: {keerthanaAcc.GetBalance()}");
+            //keerthanaAcc.Balance = 45; //can be changed from outside (if balance is public)
+                                        //cant be changed here, if made private to class
+            Console.WriteLine($"Keerthana Acc balance: {keerthanaAcc.Balance}");
         }
     }
 }
