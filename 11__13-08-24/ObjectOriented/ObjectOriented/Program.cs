87e25f02f5ece2b07bea01be15dffb2b093cd161
Keerthana <keerthanajayalalk@gmail.com>
2024-08-13 12:25:31 +0530

    Object Oriented

---
+++ 11__13-08-24/ObjectOriented/ObjectOriented/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOriented
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Trainer mahesh = new Trainer();
+            mahesh.id = 10;
+            mahesh.name = "Mahesh";
+            mahesh.skill = "C#";
+            mahesh.place = "Mysore";
+            mahesh.salary = 9000;
+            Trainer mishel = new Trainer();
+            mishel.id = 20;
+            mishel.name = "Mishel";
+            mishel.skill = "WPF";
+            mishel.place = "Idukki";
+            mishel.salary = 11000;
+
+            Console.WriteLine($"Mahesh : {mahesh.id}, {mahesh.name}, {mahesh.skill}, {mahesh.place}, {mahesh.salary}");
+            Console.WriteLine($"Mishel : {mishel.id}, {mishel.name}, {mishel.skill}, {mishel.place}, {mishel.salary}");
+        }
+    }
+}
---
+++ 11__13-08-24/ObjectOriented/ObjectOriented/Trainer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace ObjectOriented
+{
+    internal class Trainer
+    {
+        public int id;
+        public string name;
+        public string skill;
+        public string place;
+        public int salary;
+    }
+}
