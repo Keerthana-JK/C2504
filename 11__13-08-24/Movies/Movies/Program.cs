2ab93cd1c3c58c345bc26ceeb49d819c8cc621a6
Keerthana <keerthanajayalalk@gmail.com>
2024-08-13 14:50:12 +0530

    OOPS
    Movies

---
+++ 11__13-08-24/Movies/Movies/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Movies
+{
+    class Movie
+    {
+        public int id;
+        public string name;
+        //parameterized ctor
+        public Movie(int _id, string _name)
+        {
+            id = _id;
+            name = _name;
+        }
+        //non-parameterized ctor
+        public Movie()
+        {
+            id = 0;
+            name = "";
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie manjummelBoys = new Movie(1001, "Manjummel Boys");
+            Movie aavesham = new Movie();
+            aavesham.id = 1002;
+            aavesham.name = "Aavesham";
+
+            Console.WriteLine($"id={manjummelBoys.id}, name={manjummelBoys.name}");
+            Console.WriteLine($"id={aavesham.id}, name={aavesham.name}");
+        }
+    }
+}
