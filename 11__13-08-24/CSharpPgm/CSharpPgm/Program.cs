82318a24480241e31658e24e75ee3aac4600ef44
Keerthana <keerthanajayalalk@gmail.com>
2024-08-13 12:25:14 +0530

    changes

---
+++ 11__13-08-24/CSharpPgm/CSharpPgm/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using VBPgm;
+
+namespace CSharpPgm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10, b = 5;
+            int s = VB.FindSum(a, b);
+            Console.WriteLine(s);
+        }
+    }
+}
---
+++ 11__13-08-24/CSharpPgm/VBPgm/VB.vb
@@ -0,0 +1,7 @@
+﻿Public Class VB
+    Public Shared Function FindSum(first As Integer, second As Integer) As Integer
+        Dim sum As Integer = first + second
+        Return sum
+    End Function
+
+End Class
