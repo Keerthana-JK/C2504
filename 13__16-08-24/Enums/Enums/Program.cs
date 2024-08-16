6e7ecb00460a779126fb93229bd758e3cd77b0af
Keerthana <keerthanajayalalk@gmail.com>
2024-08-16 17:57:12 +0530

    OOPS - Inheritance, toString(), enum

---
+++ 13__16-08-24/Enums/Enums/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace Enums
+{
+    internal class Program
+    {
+        enum Days
+        {
+            Monday = 1, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+        }
+        enum Conversions
+        {
+            Upper, Lower, Trim
+        }
+        static void Convert(string text, Conversions action)
+        {
+            if (action == Conversions.Upper)
+                Console.WriteLine(text.ToUpper());
+            else if (action == Conversions.Lower)
+                Console.WriteLine(text.ToLower());
+            else if (action == Conversions.Trim)
+                Console.WriteLine(text.Trim());
+
+            //switch(action)
+            //{
+            //    case Conversions.Upper:
+            //        Console.WriteLine(text.ToUpper());
+            //        break;
+            //    case Conversions.Lower: 
+            //        Console.WriteLine(text.ToLower());  
+            //        break;
+            //     case Conversions.Trim:
+            //        Console.WriteLine(text.Trim());
+            //        break;
+            //}
+        }
+        static void Main()
+        {
+
+            //Days
+            Days today = Days.Monday;
+            switch (today)
+            {
+                case Days.Monday:
+                    Console.WriteLine("It's Monday, back to work!");
+                    break;
+                case Days.Tuesday:
+                    Console.WriteLine("It's Tuesday, let's keep the momentum going!");
+                    break;
+                case Days.Wednesday:
+                    Console.WriteLine("It's Wednesday, halfway through the week!");
+                    break;
+                case Days.Thursday:
+                    Console.WriteLine("It's Thursday, the weekend is near!");
+                    break;
+                case Days.Friday:
+                    Console.WriteLine("It's Friday, the weekend starts tomorrow!");
+                    break;
+                case Days.Saturday:
+                    Console.WriteLine("It's Saturday, time to relax!");
+                    break;
+                case Days.Sunday:
+                    Console.WriteLine("It's Sunday, rest up for the week ahead.");
+                    break;
+                default:
+                    Console.WriteLine("Invalid day.");
+                    break;
+            }
+            Convert("   Hello  ", Conversions.Upper);
+            Convert("  Hello   ", Conversions.Lower);
+            Convert("   Hello  ", Conversions.Trim);
+        }
+    }
+}
---
+++ 13__16-08-24/InheritanceDogAnimal/InheritanceDogAnimal/Program.cs
@@ -0,0 +1,80 @@
+﻿using InheritanceDogAnimal;
+using System;
+using System.Runtime.InteropServices.ComTypes;
+
+
+namespace InheritanceDogAnimal
+{
+    public abstract class Animal
+    {
+        public abstract void Eat();
+
+    }
+
+    public class Dog : Animal
+    {
+        public override void Eat()
+        {
+            Console.WriteLine("Eating");
+        }
+        public void Bark()
+        {
+            Console.WriteLine("Barking...");
+        }
+    }
+    public class Puppy : Dog
+    {
+        public void Weep()
+        {
+            Console.WriteLine("Weep...");
+        }
+    }
+    public class Cat : Animal
+    {
+        public override void Eat()
+        {
+            Console.WriteLine("Eating");
+        }
+        public void Meow()
+        {
+            Console.WriteLine("Meowing...");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***Dog***");
+            Dog dog = new Dog();
+            dog.Eat();
+            dog.Bark();
+            Console.WriteLine();
+            Console.WriteLine("***Puppy***");
+            Puppy puppy = new Puppy();
+            puppy.Weep();
+            puppy.Eat();
+            puppy.Bark();
+            Console.WriteLine();
+            Console.WriteLine("***Cat***");
+            Cat cat = new Cat();
+            cat.Meow();
+            cat.Eat();
+            //Console.WriteLine(cat.Equals(dog));
+            
+        }
+    }
+}
+
+//Ouput :
+//***Dog * **
+//Eating...
+//Barking...
+
+//***Puppy * **
+//Weep...
+//Eating...
+//Barking...
+
+//***Cat * **
+//Meowing...
+//Eating...
