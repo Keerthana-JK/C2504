a9e632abf73050bdb8f5d189ffde101465e47f64
Keerthana <keerthanajayalalk@gmail.com>
2024-08-17 16:30:45 +0530

    OOPS

---
+++ 13__17-08-24/Interface/Interface/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Interface
+{
+    public abstract class Flyable
+    {
+        public abstract void Fly();
+    }
+    public class Crow : Flyable
+    {
+        public override void Fly()
+        {
+            Console.WriteLine("Crow is flying");
+        }
+        SqlConnection conn;
+        SqlCommand cmd;
+        SqlDataReader rdr;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Flyable f = new Flyable();// instance of abstract/interface cant be created
+            Crow c = new Crow();
+            c.Fly();
+        }
+    }
+}
