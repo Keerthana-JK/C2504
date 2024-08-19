0143acff5d346708ea59c410435cdcfc92816699
Keerthana <keerthanajayalalk@gmail.com>
2024-08-19 18:23:19 +0530

    Update

---
+++ 14__19-08-24/InterfaceInNewCSharp.cs
@@ -0,0 +1,49 @@
+﻿//Interface in new C# (8.0 or above)
+using System;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        var FootBallPlayer = new FootBallPlayer();
+        FootBallPlayer.Play();
+        FootBallPlayer.Out();
+
+        var BasketBallPlayer = new BasketBallPlayer();
+        BasketBallPlayer.Play();
+        BasketBallPlayer.Out();
+    }
+}
+
+public interface IPlayer
+{
+    public void Play();
+    public void Out() // allows method definition //not possible in Older version
+    {
+        Console.WriteLine("Stop Playing!!!");
+    }
+}
+
+public class FootBallPlayer : IPlayer
+{
+    public void Play()
+    {
+        Console.WriteLine("FootBall Player is playing");
+    }
+    public void Out()
+    {
+        Console.WriteLine("FB player is out");
+    }
+}
+
+public class BasketBallPlayer : IPlayer
+{
+    public void Play()
+    {
+        Console.WriteLine("BasketBall Player is playing");
+    }
+    public void Out()
+    {
+        Console.WriteLine("FB player is out");
+    }
+}
---
+++ 14__19-08-24/Program.cs
@@ -0,0 +1,53 @@
+﻿//Interface in Older C# (Before 8.0)
+
+using System;
+
+namespace InterfaceInOldCSharp
+{
+    public interface IPlayer
+    {
+        void Play();//access modifier is not valid here for methods in interface (public void Play() not permitted)
+        void Out(); // Default inteface implementation is not allowed in Older C# (cannot have method definition)
+    }
+
+    public class FootBallPlayer : IPlayer
+    {
+        public void Play()
+        {
+            Console.WriteLine("FootBall Player is playing");
+        }
+        public void Out()
+        {
+            Console.WriteLine("FB player is out");
+        }
+    }
+
+    public class BasketBallPlayer : IPlayer
+    {
+        public void Play()
+        {
+            Console.WriteLine("BasketBall Player is playing");
+        }
+        public void Out()
+        {
+            Console.WriteLine("FB player is out");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Hello, World!");
+            var FootBallPlayer = new FootBallPlayer();
+            FootBallPlayer.Play();
+            FootBallPlayer.Out();
+
+            var BasketBallPlayer = new BasketBallPlayer();
+            BasketBallPlayer.Play();
+            BasketBallPlayer.Out();
+
+        }
+    }
+}
