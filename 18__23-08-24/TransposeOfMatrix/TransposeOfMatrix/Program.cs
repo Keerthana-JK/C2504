1d2f6f5b783890dd44576d4421a435e1a953bba6
Keerthana <keerthanajayalalk@gmail.com>
2024-08-25 15:56:44 +0530

    Transpose of a Matrix

---
+++ 18__23-08-24/TransposeOfMatrix/TransposeOfMatrix/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TransposeOfMatrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] a = new int[3,3]
+            {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9},
+            };
+
+            int[,] transpose = new int[3, 3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                {
+                    transpose[i,j] = a[j,i];
+                }
+            }
+
+            for(int i = 0;i < 3; i++)
+            {
+                for (int j = 0;j < 3; j++)
+                {
+                    Console.Write(transpose[i,j]+"\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
