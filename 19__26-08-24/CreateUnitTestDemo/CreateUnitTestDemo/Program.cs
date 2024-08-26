6ea6d98f5b1c0850c95041480361a9ca0b73a39f
Keerthana <keerthanajayalalk@gmail.com>
2024-08-26 13:08:17 +0530

    Create Unit Test Demo

---
+++ 19__26-08-24/CreateUnitTestDemo/CreateUnitTestDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateUnitTestDemo
+{
+    public class Calc
+    {
+        public double Multiply(double first, double second)
+        {
+            return first * second;
+        }
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Calc calc = new Calc();
+                calc.Multiply(1, 2);
+            }
+        }
+    }
+}
---
+++ 19__26-08-24/CreateUnitTestDemo/CreateUnitTestDemoTests/CalcTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CreateUnitTestDemo;
+
+
+namespace CreateUnitTestDemo.Tests
+{
+    [TestClass()]
+    public class CalcTests
+    {
+        [TestMethod()]//attribute
+        public void MultiplyTest_Return100for5and20()
+        {
+            Calc calc = new Calc();
+            double product = calc.Multiply(5.0, 20.0);
+
+            Assert.AreEqual(100.0, product);
+        }
+
+        [TestMethod()]
+        public void MultiplyTest_Return0for5and0()
+        {
+            Calc calc = new Calc();
+            double product = calc.Multiply(5.0, 0);
+
+            Assert.AreEqual(0.0, product);
+        }
+
+        [TestMethod()]
+        public void MultiplyTest_Return5for5and1()
+        {
+            Calc calc = new Calc();
+            double product = calc.Multiply(5.0, 1.0);
+
+            Assert.AreEqual(5.0, product);
+        }
+    }
+}
