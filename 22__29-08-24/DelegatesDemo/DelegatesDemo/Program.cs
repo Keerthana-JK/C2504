0ff2abbb2234c19bb86a039e76b074a74715fbb6
Keerthana <keerthanajayalalk@gmail.com>
2024-08-29 22:21:39 +0530

    Delegates

---
+++ 22__29-08-24/DelegatesDemo/DelegatesDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesDemo
+{
+    delegate void DWriteLine(string str);
+    internal class Program
+    {
+        //invokes Print("Print fn called"),
+        //which in turn calls Console.WriteLine("Print fn called").
+        static void Print(string a)//matches the DWriteLine delegate signature
+        {
+            Console.WriteLine(a);
+        }
+
+        static void PrintLetterOneByOne(string x)
+        {
+            foreach(var letter in x)
+            {
+                Console.WriteLine(letter);
+            }
+        }
+        static void Main(string[] args)
+        {
+            DWriteLine wr = Console.WriteLine; //=new DWriteLine(Console.WriteLine);
+            wr("Hello");
+            wr("Hi");
+            DWriteLine rPrint = Print;
+            rPrint("Print fn called");
+
+            DWriteLine rPrintLettersOneByOne = PrintLetterOneByOne;
+            rPrintLettersOneByOne("Object Oriented Programming");
+
+        }
+    }
+}
