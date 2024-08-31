e630065d25635461ec9a41c255d688454c787c75
Keerthana <keerthanajayalalk@gmail.com>
2024-08-31 12:54:11 +0530

    Adapter - Creational Design Pattern

---
+++ 24_31-08-24/Adapter_Creational_DesignPattern/Adapter_Creational_DesignPattern/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter_Creational_DesignPattern
+{
+    internal class Program
+    {
+        // Adaptee class (legacy system)
+        public class OldLogger
+        {
+            public void LogMessage(string message) //needs to be adapted
+            {
+                Console.WriteLine("Old Logger: " + message);
+            }
+        }
+
+        // Target interface 
+        public interface ILogger //interface that the client code expects to use
+                                // = mobile
+        {
+            void Log(string message);
+        }
+
+        // Adapter class
+        public class LoggerAdapter : ILogger //implements the `ILogger`
+                                             //uses an instance of `OldLogger` to fulfill the `Log` method's requirement
+                                             //translates the call from `Log` to `LogMessage`
+        {
+            private readonly OldLogger _oldLogger;
+
+            public LoggerAdapter(OldLogger oldLogger)
+            {
+                _oldLogger = oldLogger;
+            }
+
+            public void Log(string message)
+            {
+                // Adapt the method call to the old logger's method
+                _oldLogger.LogMessage(message);
+            }
+        }
+        static void Main(string[] args)
+        {
+            // Existing client code expects ILogger interface
+            ILogger logger = new LoggerAdapter(new OldLogger());
+            logger.Log("This is a log message.");
+        }
+    }
+}
