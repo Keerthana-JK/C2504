1b3cc00477c90609f47f269434f6e4b2636171b7
Keerthana Jayalal K <71508789+Keerthana-JK@users.noreply.github.com>
2024-10-03 18:30:06 +0530

    Add files via upload

---
+++ Phase 2/ConsoleAppInterface/ConsoleAppInterface/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppInterface
+{
+	internal class Program
+	{
+		interface IA 
+		{
+			void Show();
+		}
+		interface IB : IA { }
+		interface IC : IA { }
+		public class X : IB, IC
+		{
+			public void Show()
+			{
+				System.Console.WriteLine("from IA");
+			}
+		}
+		static void Main(string[] args)
+		{
+			X x = new X();
+			x.Show();
+		}
+	}
+}
