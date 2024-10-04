5a397a19a8c8d1e9ea5206353604baec1356a23b
Keerthana Jayalal K <71508789+Keerthana-JK@users.noreply.github.com>
2024-10-04 09:03:41 +0530

    Add files via upload

---
+++ Phase 2/WpfCounterApp/WpfCounterApp/CounterConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfCounterApp
+{
+	public static class CounterConfig
+	{
+		//forms 
+		public static Window FrmNewCounter { get; set; }
+
+
+		//view model
+		public static CounterViewMode VueModel { get; set; }
+
+		static CounterConfig()
+		{
+			VueModel = new CounterViewMode();
+			FrmNewCounter = new MainWindow();
+		}
+	}
+}
