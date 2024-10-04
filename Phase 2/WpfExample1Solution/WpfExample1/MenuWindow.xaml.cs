c5a3dee9b98d7b6110b6624ca789f33da5409841
Keerthana Jayalal K <71508789+Keerthana-JK@users.noreply.github.com>
2024-10-04 09:04:51 +0530

    Add files via upload

---
+++ Phase 2/WpfExample1Solution/WpfExample1/MenuWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfExample1
+{
+	/// <summary>
+	/// Interaction logic for MenuWindow.xaml
+	/// </summary>
+	public partial class MenuWindow : Window
+	{
+		public MenuWindow()
+		{
+			InitializeComponent();
+		}
+		private MainWindow mainWindow = new MainWindow();
+		private SumCalcWindow sumCalcWindow = new SumCalcWindow();
+		private SquareCubeCalcWindow squareCubeCalcWindow=new SquareCubeCalcWindow();
+		private void btnCounter_Click(object sender, RoutedEventArgs e)
+		{
+			mainWindow.Show();
+			mainWindow.Activate();
+		}
+
+		private void btnSumCalc_Click(object sender, RoutedEventArgs e)
+		{
+			sumCalcWindow.Show();
+			sumCalcWindow.Activate();
+		}
+
+		private void btnSquareCube_Click(object sender, RoutedEventArgs e)
+		{
+			squareCubeCalcWindow.Show();
+			squareCubeCalcWindow.Activate();
+		}
+
+		private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+		{
+			e.Cancel = true;
+			this.Hide();
+		}
+	}
+}
---
+++ Phase 2/WpfExample1Solution/WpfExample1/SquareCubeCalcWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfExample1
+{
+	/// <summary>
+	/// Interaction logic for SquareCubeCalcWindow.xaml
+	/// </summary>
+	public partial class SquareCubeCalcWindow : Window
+	{
+		public SquareCubeCalcWindow()
+		{
+			InitializeComponent();
+		}
+		private bool IsNum(string numStr)
+		{
+			try
+			{
+				int.Parse(numStr);
+				return true;
+			}
+			catch
+			{
+				return false;
+			}
+		}
+
+		private void btnFindSquare_Click(object sender, RoutedEventArgs e)
+		{
+
+			if (!IsNum(txtNumber.Text))
+			{
+				MessageBox.Show("Please enter only number");
+				txtNumber.Focus();
+				txtNumber.SelectAll();
+
+				return;
+			}
+			int num = int.Parse(txtNumber.Text);
+			int square = num * num;
+			txtResult.Text = square.ToString();
+			lblResult.Text = "Square";
+		}
+
+		private void btnFindCube_Click(object sender, RoutedEventArgs e)
+		{
+			if (!IsNum(txtNumber.Text))
+			{
+				MessageBox.Show("Please enter only number");
+				txtNumber.Focus();
+				txtNumber.SelectAll();
+
+				return;
+			}
+			int num = int.Parse(txtNumber.Text);
+			int cube = num * num * num;
+			txtResult.Text = cube.ToString();
+			lblResult.Text = "Cube";
+
+
+		}
+
+		private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+		{
+			e.Cancel = true;
+			this.Hide();
+		}
+	}
+}
